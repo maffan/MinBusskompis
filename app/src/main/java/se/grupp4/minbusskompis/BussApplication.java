7b2455f152b3074af0cc90e818065de9e07c724e
maffan <marcus.flyckt@gmail.com>
2015-10-12 11:37:18 +0200

    lade till lite testkod

--- app/src/main/java/se/grupp4/minbusskompis/BussApplication.java
+++ app/src/main/java/se/grupp4/minbusskompis/BussApplication.java
@@ -3,8 +3,12 @@ package se.grupp4.minbusskompis;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseInstallation;
-import com.parse.ParsePush;
+
+import se.grupp4.minbusskompis.parsebuss.AsyncTaskCompleteCallback;
+import se.grupp4.minbusskompis.parsebuss.BussData;
+import se.grupp4.minbusskompis.parsebuss.BussDestination;
 
 /**
  * Created by Marcus on 9/29/2015.
@@ -14,5 +18,11 @@ public class BussApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Parse.initialize(this);
+        BussData.getInstance().fetchData(new AsyncTaskCompleteCallback() {
+            @Override
+            public void done() {
+                BussData.getInstance().addDestinationToChild(new BussDestination(new ParseGeoPoint(0, 0), "Saxofonlektion"), ParseInstallation.getCurrentInstallation().getInstallationId());
+            }
+        });
     }
 }
